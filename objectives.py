f352750d603330970e8f1b8a5a3a8ca237eef64a
Jonas <Jonas.Degrave@UGent.be>
2016-10-18 17:39:13 +0200

    Added custom auc objective

--- objectives.py
+++ objectives.py
@@ -1,4 +1,9 @@
-import theano, theano.tensor as T
+import theano
+import theano.tensor as T
+try:
+    from theano.tensor.extra_ops import searchsorted
+except:
+    raise "FUCKING UPDATE YOUR THEANO TO VERSION ZERO POINT FUCKING NINE"
 import numpy as np
 import pandas as pd
 import lasagne
@@ -469,3 +474,128 @@ def bc_with_ranking(y_prediction, y_true):
     # return (rankloss + 1) * logloss - an alternative to try
     #return rankloss + logloss
     return logloss
+
+
+
+
+#================ Turd objective ==================
+
+MAX_INT = np.iinfo(np.int32).max
+
+
+class JonasAUCobjective():
+
+    #TODO: remove bias from 2 original values at infinity
+
+    def __init__(self):
+        """
+        These must be kept sorted
+        """
+        self.T = theano.shared(value=np.array([-np.inf, np.inf], dtype='float32'), name='T')
+        self.labels = theano.shared(value=np.array([0, 1], dtype='float32'), name='labels')
+        self.TPR = theano.shared(value=np.array([1, 1], dtype='float32'), name='TPR')
+        self.FPR = theano.shared(value=np.array([1, 0], dtype='float32'), name='FPR')
+        self.time_added = np.array([MAX_INT, MAX_INT], dtype='int32')
+        """
+        These contain some global information on the previous lists
+        """
+        self.AUC0 = theano.shared(value=np.float32(1.), name='AUC0')
+        self.N_P = theano.shared(value=np.float32(1.), name='N_P')
+        self.N_F = theano.shared(value=np.float32(1.), name='N_F')
+        """
+        These contain some information for maintaining the lists
+        """
+        self.N_added = 2
+
+
+    def auc_error(self, T_prediction, true_label):
+        # get TPR1, TPR2, label_1, FPR1, T1, T2 from the estimated distribution
+        idx = searchsorted(self.T, T_prediction, side='left')
+        TPR1 = self.TPR[idx-1]
+        TPR2 = self.TPR[idx]
+        label_1 = self.labels[idx-1]
+        label_2 = self.labels[idx]
+        FPR1 = self.FPR[idx-1]
+        T1 = self.T[idx-1]
+        T2 = self.T[idx]
+
+        f1 = 1 - label_1
+        f2 = 1 - label_2
+        l = true_label
+        f = 1-true_label
+        AUC1 = self.AUC0 + f1*l + f*TPR1 + l*(self.N_F-FPR1)
+        dAUC = (TPR2 - TPR1)*f + f2*l
+
+        # deal with new parameters at edge of list of parameters
+        # note: they never have a gradient?
+        coef = T.switch(T.isinf(T1), T.switch(T.isinf(T2), 0.5, 1.0),
+                                     T.switch(T.isinf(T2), 0.0, (T_prediction-T1)/(T2-T1)))
+        coef = (T_prediction-T1)/(T2-T1)
+        AUCt = AUC1 + coef*dAUC
+
+        return AUCt / ((self.N_P+l) * (self.N_F+f))
+
+
+    def add_points(self, predicted, label, never_remove=False):
+        idx = np.searchsorted(self.T.get_value(), predicted)
+        self.T.set_value(np.insert(self.T.get_value(), idx, predicted,))
+        labels = np.insert(self.labels.get_value(), idx, label,)
+        self.labels.set_value(labels)
+        self.TPR.set_value(np.cumsum(labels[::-1])[::-1])
+        self.FPR.set_value(np.arange(1,len(labels)+1, dtype='float32')[::-1] - np.cumsum(labels[::-1])[::-1])
+        if never_remove:
+            self.time_added = np.insert(self.time_added, idx, MAX_INT)
+        else:
+            self.time_added = np.insert(self.time_added, idx, self.N_added)
+
+        self.AUC0.set_value(np.sum((1-labels) * np.cumsum(labels[::-1])[::-1]))
+        self.N_P.set_value(np.sum(labels))
+        self.N_F.set_value(np.sum(1-labels))
+
+        if isinstance(predicted, list):
+            self.N_added += len(self.predicted)
+        else:
+            self.N_added += 1
+
+
+    @property
+    def current_auc(self):
+        return self.AUC0.get_value() / (self.N_P.get_value() * self.N_F.get_value())
+
+
+    def print_status(self):
+        print "T:  \t", self.T.get_value()
+        print "lbl:\t", self.labels.get_value()
+        print "TPR:\t", self.TPR.get_value()
+        print "FPR:\t", self.FPR.get_value()
+        print "AUC0:\t", self.AUC0.get_value()
+        print "N_P:\t", self.N_P.get_value()
+        print "N_F:\t", self.N_F.get_value()
+        print "AUC:\t", self.current_auc
+
+
+    def __call__(self, *args, **kwargs):
+        return self.auc_error(*args, **kwargs)
+
+if __name__=="__main__":
+    aucd = JonasAUCobjective()
+
+    aucd.add_points(-1,1)
+    aucd.add_points(3,1)
+    aucd.print_status()
+
+
+    w, x, y = T.scalar('w'), T.scalar('x'), T.scalar('y')
+    z1 = aucd(w*x, y)
+    f = theano.function([w, x, y], [z1], on_unused_input='ignore')
+    print f(1,2,0)
+    print f(1,2,1)
+
+    gr1 = T.grad(z1, x)
+    g = theano.function([w, x, y], [gr1], on_unused_input='ignore')
+    print g(1,2,0)
+    print g(1,2,1)
+
+
+
+
